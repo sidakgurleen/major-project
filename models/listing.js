2e8dc33c6f474f2fde13123c833199accd6e34a8
sidakgurleen <gurleenkaursidak369@gmail.com>
2024-05-22 15:48:20 +0530

    add new project

---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+const listingSchema=new Schema({
+    title:{
+       type:String,
+       required:true,
+    },
+    description:String,
+    image:{
+        type:String,
+        default:"https://images.unsplash.com/photo-1578645510447-e20b4311e3ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fGNhbXBpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+        
+     set:(v)=>v==="" ?"https://images.unsplash.com/photo-1578645510447-e20b4311e3ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fGNhbXBpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+     :v,
+     },
+    price:Number,
+    location:String,
+    country:String,
+});
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
